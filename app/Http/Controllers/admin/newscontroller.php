17fa22ad773c855fe07753eeafb553344db23285
EC2 Default User <ec2-user@ip-172-31-33-232.us-east-2.compute.internal>
2020-01-14 10:19:36 +0000

    課題7

---
+++ app/Http/Controllers/admin/newscontroller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class newscontroller extends Controller
+{
+    //
+
+
+public function add ()
+ {
+    return view('admin.news.create');
+ }
+
+}
